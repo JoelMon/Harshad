2fdd1149a475010fd2c922b7144293ef4cbbd628
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-07-08 23:01:45 -0400

    impl functions for calc k's constant

--- src/kconstlib.rs
+++ src/kconstlib.rs
@@ -1,3 +1,5 @@
+use std::num;
+
 const FOUR_DIGITS: usize = 4;
 
 trait Length {
@@ -29,13 +31,50 @@ fn is_veried(number: usize) -> bool {
     !varied
 }
 
+/// Converts `usize` interger into a `Vec<usize>`
+fn to_vec(number: usize) -> Vec<usize> {
+    let item: Vec<usize> = number
+        .to_string()
+        .chars()
+        .map(|i| String::from(i).parse().unwrap())
+        .collect();
+    item
+}
+
+/// Converts `Vec<usize>` to `usize`
+fn to_usize(number: Vec<usize>) -> usize {
+    let number: String = number.iter().map(|x| x.to_string()).collect();
+    number
+        .parse()
+        .expect("number was not able to be parsed into a usize")
+}
+
 /// Sorts the all digits from largest to smallest
-fn sort(number: usize) -> usize {
-    todo!()
+fn sort_asend(number: usize) -> usize {
+    let mut num = to_vec(number);
+    num.sort();
+    to_usize(num)
+}
+
+/// Sorts the all digits from smallest to biggest
+fn sort_desend(number: usize) -> usize {
+    let mut num = to_vec(number);
+    num.sort_by(|a, b| b.cmp(a));
+    to_usize(num)
+}
+
+/// Takes a `usize` sorts it and subtracts
+fn subtract(number: usize) -> usize {
+    let num1 = sort_desend(number);
+    let num2 = sort_asend(number);
+
+    num1 - num2
 }
 
 #[cfg(test)]
 mod tests {
+
+    use std::vec;
 
     use super::*;
 
@@ -82,5 +121,45 @@ mod tests {
         let result = is_veried(number);
 
         assert_eq!(result, false);
+    }
+
+    #[test]
+    fn to_vec_pass() {
+        let number = 1234;
+        let result = to_vec(number);
+
+        assert_eq!(result, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn sort_asend_1() {
+        let number = 3512;
+        let result = sort_asend(number);
+
+        assert_eq!(result, 1235);
+    }
+
+    #[test]
+    fn sort_desend_1() {
+        let number = 3512;
+        let result = sort_desend(number);
+
+        assert_eq!(result, 5321);
+    }
+
+    #[test]
+    fn subtract_1() {
+        let number = 3512;
+        let result = subtract(number);
+
+        assert_eq!(result, 4086);
+    }
+
+    #[test]
+    fn subtract_2() {
+        let number = 9919;
+        let result = subtract(number);
+
+        assert_eq!(result, 7992);
     }
 }
