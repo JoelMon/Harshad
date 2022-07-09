e2fe79e6ca37d7321501ff1322f1befa9d4dd012
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-07-08 20:31:52 -0400

    Implemented is_veried()

--- src/kconstlib.rs
+++ src/kconstlib.rs
@@ -1,3 +1,5 @@
+const FOUR_DIGITS: usize = 4;
+
 trait Length {
     fn len(&self) -> usize;
 }
@@ -10,10 +12,23 @@ impl Length for usize {
 
 /// Checks if the number entered contains four digits.
 fn is_four_digits(number: usize) -> bool {
-    if number.len() == 4 {
+    if number.len() == FOUR_DIGITS {
         return true;
     }
     false
+}
+
+/// Checks to see if the digits varies, ie. all the digits passed in are not the same.
+fn is_veried(number: usize) -> bool {
+    let items: Vec<char> = number.to_string().chars().map(|x| x).collect();
+
+    let first = items[0];
+    let varied: bool = items.iter().all(|&char| char == first);
+
+    match varied {
+        true => false,
+        false => true,
+    }
 }
 
 /// Sorts the all digits from largest to smallest
@@ -44,6 +59,30 @@ mod tests {
     fn is_four_digits_false_6() {
         let number = 123456;
         let result = is_four_digits(number);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn is_veried_true() {
+        let number = 1234;
+        let result = is_veried(number);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn is_veried_true2() {
+        let number = 1112;
+        let result = is_veried(number);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn is_veried_false() {
+        let number = 1111;
+        let result = is_veried(number);
+
         assert_eq!(result, false);
     }
 }
